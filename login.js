ead3957a30674cb08fd6a6da310d440300361bb3
Rafi Ahmad As Sidiq <rafiahmadassidiq@gmail.com>
2022-04-11 16:15:44 +0700

    fx

--- login.js
+++ login.js
@@ -14,7 +14,24 @@ function generateToken(data){
 async function login(userlogin, password) {
     try{
         let pool = await sql.connect(configPortal);
-        let login = await pool.request().input('userlogin', sql.VarChar, userlogin).input('password', sql.VarChar, password).query("SELECT '01' as m_program, a.m_login as loginid, b.m_nik as nik, c.m_foto as profile, b.m_nama as nama, b.m_cabang as cabang, b.m_departemen as departemen, b.m_divisi as divisi, b.m_subdivisi as subdivisi, b.m_golkar as level, a.m_brand as brand, a.m_lokasi as store, a.m_stock as stock, a.m_divisi2 as groupuser, b.m_lokasi as lokasi, b.m_subdivisinew as subdivisinew, convert(varchar(20),b.m_tglkeluar,103) as cotglkeluar, b.m_tglkeluar as tglkeluar, a.m_status as status FROM msuser a, dbhrd.dbo.mskaryawan b, dbhrd.dbo.msdetilkaryawan c WHERE a.m_nik = b.m_nik AND b.m_nik = c.m_nik AND UPPER(a.m_login) = @userlogin AND PWDCOMPARE(@password, m_newpass) = 1");
+        let login = await pool.request().input('userlogin', sql.VarChar, userlogin).
+        input('password', sql.VarChar, password).
+        query(`SELECT '01' as m_program, a.m_login as loginid, 
+        b.m_nik as nik, c.m_foto as profile, 
+        b.m_nama as nama, b.m_cabang as cabang,
+         b.m_departemen as departemen, b.m_divisi as divisi, 
+         b.m_subdivisi as subdivisi, b.m_golkar as level, 
+         a.m_brand as brand, a.m_lokasi as store, 
+         a.m_stock as stock, a.m_divisi2 as groupuser, 
+         b.m_jabatan,
+         b.m_lokasi as lokasi, b.m_subdivisinew as subdivisinew, 
+         convert(varchar(20),b.m_tglkeluar,103) as cotglkeluar, 
+         b.m_tglkeluar as tglkeluar, a.m_status as status FROM 
+         msuser a, dbhrd.dbo.mskaryawan b, 
+         dbhrd.dbo.msdetilkaryawan c 
+         WHERE a.m_nik = b.m_nik AND b.m_nik = c.m_nik 
+         AND UPPER(a.m_login) = @userlogin AND 
+         PWDCOMPARE(@password, m_newpass) = 1`);
         return  login.recordsets;
     }catch(error){
         console.log(error);
